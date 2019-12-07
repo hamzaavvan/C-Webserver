b115767388fc8c8d67ae82edd96e68cb8f8031d0
Hamza Avvan <hamzaavvan@gmail.com>
2019-12-07 19:51:11 +0500

    Code Update

--- Code/webserver.c
+++ Code/webserver.c
@@ -10,32 +10,144 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <regex.h>
 
-char webpage[] = 
-"HTTP/1.1 200 OK\r\n"
-"Content-Type: text/html; charset=UTF-8\r\n\r\n";
+#define req_len 2048
+
+#define HEADER_SZ 500
+#define STATUS_SZ 20
+
+struct ext
+{
+	char name[100];
+	char value[100];
+};
+
+// List of server's supported extensions 
+struct ext extensions[] = {
+	{"html", "text/html; charset=UTF-8"}, {"css", "text/css"},
+ 	{"js", "application/js"}, {"jpg", "image/jpeg"},
+ 	{"png", "image/png"},  
+};
+
+int ext_len = sizeof(extensions)/sizeof(extensions[0]);
+char resp_headers[] = "HTTP/1.1";
+
+
+void prepare_status(FILE *fd, char *status) {
+	if (fd == NULL) {
+		strcpy(status, " 404 Not Found\r\n");
+	} else {
+		strcpy(status, "  200 OK\r\n");
+	}
+}
+
+void prepare_content_type(FILE *fd, char file[], char *content_type) {
+	char *token = strtok(file, ".");
+	char ext[10];
+
+	while(token != NULL) {
+		strcpy(ext, token);
+		token = strtok(NULL, ".");
+	}
+
+	for (int i = 0; i < ext_len; ++i)
+	{
+		if (!strcmp(ext, extensions[i].name)  || fd == NULL) {
+			strcat(content_type, extensions[i].value);
+			break;
+		}
+	}
+
+	strcat(content_type, "\r\n");
+}
+
+void prepare_headers(FILE *fd, char file[], int length) {
+	char sign[] = "Server: C/1.0 \r\n";
+	char split[] = "\r\n";
+	
+	char content_length[50];
+	sprintf(content_length, "%s%d%s", "Content-Length: ", length, "\r\n");
+
+	char status[STATUS_SZ];
+	prepare_status(fd, status);
+
+	char content_type[100] = "Content-type: ";
+	prepare_content_type(fd, file, content_type);
+
+	strcat(resp_headers, status);
+	strcat(resp_headers, sign);
+	strcat(resp_headers, content_type);
+	strcat(resp_headers, content_length);
+	strcat(resp_headers, split); // to split headers and bodh
+}
+
+
+void prepare_response(int client, char file[]) {
+	FILE *op = fopen(file, "rb");
+	FILE *dup_op = op;
+
+	if (op == 0)
+		op = fopen("404.html", "r");
+
+	fseek(op, 0, SEEK_END);
+
+	long fsize = ftell(op);
+	unsigned char content[fsize+1]; // an extra byte for null byte 
+
+	prepare_headers(dup_op, file, sizeof(content)-1);
+
+	fseek(op, 0, SEEK_SET);
+	fread(content, 1, fsize, op);
+	fclose(op);
+
+	content[fsize] = '\0'; // setting null byte to extra byte for terminating string
+
+	write(client, resp_headers, strlen(resp_headers));
+	write(client, content, fsize);
+}
+
+void process_request(char req[req_len], int fd_client, char path[]) {
+	char *token = strtok(req, "\n\r");
+
+	char header[HEADER_SZ];
+	strcpy(header, token);
+
+	char *tokens = strtok(header, " ");
+	
+	char method[10];
+	strcpy(method, tokens);
+
+	tokens = strtok(NULL, " ");
+	char file[strlen(tokens)];
+	sprintf(file, "%s%s", path, tokens);
+
+	prepare_response(fd_client, file);
+
+	close(fd_client);
+}
 
 int main(int argc, char const *argv[])
 {
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t sin_len = sizeof(client_addr);
 	int fdimg, fd_server, fd_client;
-	char buf[2048];
-	int on =1;
-	int port;
+	char req[req_len], path[100] = ".";
+	int on = 1, port;
 
 	fd_server = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (argc < 2)
-	{
+	if (argc < 2) {
 		printf("Port error! Please specify a port\n");
 		exit(1);
 	}
 
 	port = atoi(argv[1]);
-	
-	if(fd_server < 0)
-	{
+
+	if (argc > 2)
+		strcpy(path, argv[2]);
+
+	if(fd_server < 0) {
 		perror("socket");
 		exit(1);
 	}
@@ -61,36 +173,27 @@ int main(int argc, char const *argv[])
 		exit(1);
 	}
 
-	printf("Server started\n");
+	printf("Server started\n\n");
+
 	while(1)
 	{
 		fd_client = accept(fd_server, (struct sockaddr *) &client_addr, &sin_len);
-		if(fd_client == -1)
-		{
+		if(fd_client == -1) {
 			perror("connection error");
 			continue;
 		}
 
-		// printf("got client connection\n");
-		if(!fork())
-		{
-			// close(fd_server);
-			// memset(buf, 0, 2048);
-			// read(fd_client, buf, 2047);
+		if(!fork()) {
+			close(fd_server);
+			memset(req, 0, 2048);
+			read(fd_client, req, 2047);
 
-			// // printf("%s\n", buf);
-			// if(strncmp(buf, "GET /doctest.png", 16))
-			// {
-			// 	// int webfd = open("index.html", O_RDONLY);
-
-			// 	// write(fd_client, webpage, sizeof(webpage) -1);
-			// }
-			
-			// close(fd_client);
-			
+			printf("%s\n", req);
+			process_request(req, fd_client, path);
+					
 			// printf("closing\n");
 			
-			// exit(0);		
+			exit(0);		
 		}
 		close(fd_client);
 	}
